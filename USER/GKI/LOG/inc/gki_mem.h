1c2ae6f710fbd1f7eebcbe455eeebe06b98af22b
xumanwei <1963774470@qq.com>
2024-06-08 16:33:15 +0800

    222

---
+++ USER/GKI/LOG/inc/gki_mem.h
@@ -0,0 +1,16 @@
+//
+// Created by 许满伟 on 24-6-8.
+//
+
+#ifndef FREERTOS_SDI_GKI_MEM_H
+#define FREERTOS_SDI_GKI_MEM_H
+
+typedef unsigned long uint32_t;
+typedef unsigned short uint32_t;
+typedef unsigned char uint32_t;
+
+#define
+
+
+
+#endif //FREERTOS_SDI_GKI_MEM_H
---
+++ USER/GKI/LOG/src/gki_mem.c
@@ -0,0 +1,8 @@
+//
+// Created by 许满伟 on 24-6-8.
+//
+
+void* gki_mem_alloc(uint32_t size,const char* file_name, uint32_t line)
+{
+
+}
